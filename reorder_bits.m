9278206e620a27554bea3ae6878f48c667b39d0e
melonxi <115768975@qq.com>
2018-04-10 14:39:59 +0800

    help BP_decoder
    one part of the BP_decoder

---
+++ reorder_bits.m
@@ -0,0 +1,16 @@
+function [u]= reorder_bits(c,rearranged_cols)
+
+%outputs the codeword generated using the IG parity check matrix, H.
+%v= reorder_bits(c,rearranged_cols)
+%For examples and more details, please refer to the LDPC toolkit tutorial at
+%http://arun-10.tripod.com/ldpc/ldpc.htm
+rows=length(rearranged_cols);
+for i=rows:-1:1
+   if rearranged_cols(i)~=0
+      temp=c(i);
+      c(i)=c(rearranged_cols(i));
+      c(rearranged_cols(i))=temp;
+   end
+end
+
+u=c;
